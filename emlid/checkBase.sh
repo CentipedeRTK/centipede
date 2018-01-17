42be039d9e9329fe395c91ec20b11d8def6dc70f
ANCELIN Julien <julien.ancelin@inra.fr>
2018-01-17 12:08:51 +0100

    Update checkBase.sh

--- emlid/checkBase.sh
+++ emlid/checkBase.sh
@@ -4,6 +4,7 @@
 ### create script & do: chmod +x ./checkBase.sh
 #### create a cron job: crontab -e
 #### add: */5 * * * *  home/centipede/checkBase.sh
+
 RTCM='jancelin.ddns.xx'    ##DNS ou IP
 PORT='9000'                 ##Port de diffusion
 CONTAINER='sig_postgis'     ##container postgis
@@ -11,12 +12,13 @@ BASE='demo'                 ##base de données
 TABLE='rtk.antenne'         ##table
 PING='ping'                 ##champ boolean
 DATE='ping_date'            ##champ timestamp with time zone
-ID='id'                     ##Id de l'entité
+ID='id'                     ##champ Id de l'entité
+IDVALUE='7'
 
   if nc -z -w2 $RTCM $PORT ; then
       echo OK
-      docker exec -d $CONTAINER su postgres -c "psql $BASE -c 'update $TABLE SET $PING= true, $DATE= LOCALTIMESTAMP(0)  where $ID = 7;'"
+      docker exec -d $CONTAINER su postgres -c "psql $BASE -c 'update $TABLE SET $PING= true, $DATE= LOCALTIMESTAMP(0)  where $ID = $IDVALUE;'"
   else
       echo DOWN
-      docker exec -d $CONTAINER su postgres -c "psql $BASE -c 'update $TABLE SET $PING= false  where $ID = 7;'"
+      docker exec -d $CONTAINER su postgres -c "psql $BASE -c 'update $TABLE SET $PING= false  where $ID = $IDVALUE;'"
   fi

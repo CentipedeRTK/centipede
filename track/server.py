796c3532882bcebaeb580587c75c5228e239be82
ANCELIN Julien <julien.ancelin@inra.fr>
2021-05-04 16:02:00 +0200

    rtklib param .pos + save ip tcp client

--- track/server.py
+++ track/server.py
@@ -2,6 +2,7 @@ import socket
 import psycopg2
 import select
 import os
+from datetime import datetime
 
 t_host = os.getenv('HOST','localhost')
 t_port = os.getenv('PORT','5432')
@@ -12,15 +13,19 @@ t_pw = os.getenv('PASS','docker')
 conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_dbname, user=t_user, password=t_pw)
 cur = conn.cursor()
 
-def insertData(vL):
+def insertData(vL, ip):
     s = ""
-    s += "INSERT INTO " + os.getenv('TBL','public.llh1') +" ("
-    s += " dat,tim,lati,longi,height,q,ns,sdn,sde,sdu,age,ratio"
+    s += "INSERT INTO " + os.getenv('TBL','public.llh') +" ("
+    s += " dat,lati,longi,height,q,ns,sdn,sde,sdu,sdne,sdeu,sdun,age,ratio,ip"
     s += ") VALUES ("
-    s += "(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s)"
+    s += "(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s),(%s)"
     s += ")"
     try:
-        cur.execute(s,(vL[0],vL[1],vL[2],vL[3],vL[4],vL[5],vL[6],vL[7],vL[8],vL[9],vL[13],vL[14]))
+        #print(vL[0] + ' ' + vL[1])
+        dt_tm = str(vL[0]) + ' ' + str(vL[1])
+        date_time_obj = datetime.strptime(dt_tm, '%Y/%m/%d %H:%M:%S.%f')
+        #print(date_time_obj)
+        cur.execute(s,(date_time_obj,vL[2],vL[3],vL[4],vL[5],vL[6],vL[7],vL[8],vL[9],vL[10],vL[11],vL[12],vL[13],vL[14],ip[0]))
         conn.commit()
         #print('data inject!')
     except psycopg2.Error as e:
@@ -28,9 +33,9 @@ def insertData(vL):
         return render_template("error.html", t_msg_err = t_msg_err)
     #cur.close()
 
-def tab(vL):
+def tab(vL, ip):
     #print(str(vL)
-    insertData(vL)
+    insertData(vL, ip)
 
 def socketSetup():
     # Server socket
@@ -63,8 +68,9 @@ def socketListen(connSocket):
                              s.close()
                              clients.remove(s)
                              break
+                         ip = str(addr).replace('(','').replace(')','').replace("'",'').split(", ")
                          vL = valuesReceived.split()
-                         tab(vL)
+                         tab(vL,ip)
                      except socket.timeout:
                          print(str(addr) + "Client dead")
                          s.close()

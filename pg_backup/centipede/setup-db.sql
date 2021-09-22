59314446079eb172a128453fa4d5948ce599b598
jancelin <julien.ancelin@inra.fr>
2021-09-22 16:51:48 +0200

    modify db logs

--- pg_backup/centipede/setup-db.sql
+++ pg_backup/centipede/setup-db.sql
@@ -5,16 +5,6 @@
 -- Dumped from database version 13.2 (Debian 13.2-1.pgdg110+1)
 -- Dumped by pg_dump version 13.2 (Debian 13.2-1.pgdg110+1)
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: centipede; Type: SCHEMA; Schema: -; Owner: centipede

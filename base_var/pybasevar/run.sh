9af0a4ffbde3d21f9e7fbd3de82b030f1b311147
jancelin <julien.ancelin@inra.fr>
2021-12-17 16:11:56 +0100

    upd doc  base variable module

--- base_var/pybasevar/run.sh
+++ base_var/pybasevar/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "create /dev/pts/1 /2"
-##TODO bug when socat start from pybasegnss.py,
-socat -d -d pty,raw,echo=0 pty,raw,echo=0 &>/dev/null & Temporarily it starts here
+##TODO bug when socat start from pybasegnss.py, Temporarily it starts here
+socat -d -d pty,raw,echo=0 pty,raw,echo=0 &>/dev/null &
 sleep 3 &&
 echo "start pybasevar"
 echo "Telegram connexion > apikey :"$APIKEY " / user id" $USERID

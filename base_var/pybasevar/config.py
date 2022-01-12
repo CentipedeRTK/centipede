07c1a68d760ffb070dd8b80c29aeb2a043a4aa1f
jancelin <julien.ancelin@inra.fr>
2022-01-12 11:07:19 +0100

    check base is alive + hysteresis between base egal distance in basevar

--- base_var/pybasevar/config.py
+++ base_var/pybasevar/config.py
@@ -1,30 +1,36 @@
 import serial
 import io
+from decimal import *
 ## 00-START socat TODO
 socat="socat -d -d pty,raw,echo=0 pty,raw,echo=0 &>/dev/null"
 ## 01-Start caster
 ntripc="str2str -in serial://pts/2:115200:8:n:1:off -n 1 -b 1 -out ntripc://@:9999/ME &>/dev/null"
 pid_str=0
 ## 02-Start a generic stream RTCM3 in
-stream1="str2str -in ntrip://:@caster.centipede.fr:2101/"
+stream1="str2str -in ntrip://:@caster.centipede.fr:80/"
 stream2=" -out serial://pts/1:115200:8:n:1:off &>/dev/null"
 ## 1-Analyse nmea from gnss ntripclient for get lon lat
 ser = serial.Serial('/dev/pts/1', 115200, timeout=1)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 ## Defaut longitude
 lon = 0.0
+rlon = Decimal('-0.949')
+rlat = Decimal('46.165')
+rtime = '00:00:00'
 ## 2-Get caster sourcetable
 caster='caster.centipede.fr'
-port=2101
+port=80
 ###max search distance between rover & caster bases
 maxdist=300
 ## 3-Get variables
 ###defaut mountpoint
-mp_use = "ENSG"
-mp_alive="ENSG"
+mp_use = "CT"
+mp_alive="CT"
 ##defaut Distance between rover > base
 dist_r2mp = 500
 ###critical distance before change
 mp_km_crit = 15
 ###hysteresis
 htrs = 1
+###message
+message = "Defaut_Message"
--- base_var/pybasevar/pybasevar.py
+++ base_var/pybasevar/pybasevar.py
@@ -7,13 +7,14 @@ import os
 import signal
 import telegram
 import sys
+from decimal import *
 from ntripbrowser import NtripBrowser
 from multiprocessing import Process
 from datetime import datetime
 import config
 
 ## 00-START socat
-## TODO Open virtual ports, BUG don't run in background, use run.sh.
+## TODO : Open virtual ports, BUG don't run in background, use run.sh.
 # def socat():
 #  process1 = subprocess.Popen(config.socat.split())
 #  print("/dev/pts/1 & /2 created")
@@ -26,21 +27,6 @@ def str2str_out():
     pid2 = process2.pid
     print("STR2STR: serial 2 ntripc is runnig, pid: ",pid2)
 
-def main():
-    ## 00-START socat
-    ## TODO
-    ## 01-START caster
-    Process(target=str2str_out).start()
-    ## 02-START a generic stream RTCM3 in
-    bashstr = config.stream1+config.mp_use+config.stream2
-    str2str = subprocess.Popen(bashstr.split())
-    ## 4-Get str2str in pid
-    config.pid_str = str2str.pid
-    print("STR2STR: Defaut ntripcli 2 serial is runnig, pid: ",config.pid_str)
-    ## 03-START loop to check rover position and nearest base
-    Process(target=loop_mp).start()
-
-
 def telegrambot():
     if len(sys.argv) >= 2:
         api_key = str( sys.argv[1] )
@@ -50,9 +36,8 @@ def telegrambot():
 
 def logging():
     ##log in file
-    presentday = datetime.now()
     file = open("basevarlog.txt", "a")
-    file.write(config.message + ","+ presentday.strftime('%d-%m-%Y') +'\n')
+    file.write(config.message +'\n')
     file.close
 
 def movetobase():
@@ -70,99 +55,139 @@ def movetobase():
     str2str = subprocess.Popen(bashstr.split())
     config.pid_str = str2str.pid
     ##Metadata
+    presentday = datetime.now()
     config.message = ("Move to base ," + str(mp_use1) +","+
-    str(round(mp_use1_km,2))+","+str(round(msg.latitude,7))+","+
-    str(round(msg.longitude,7))+","+ str(msg.timestamp))
+    str(round(mp_use1_km,2))+","+str(round(config.rlat,7))+","+
+    str(round(config.rlon,7))+","+ presentday.strftime('%Y-%m-%d') +" "+str(config.rtime))
     ##Log data in file
-    logging()
+    #logging()
     ##Send message to Telegram if param exist
-    telegrambot()
+    #telegrambot()
 
-## 03-START loop to check rover position and nearest base
+def ntripbrowser():
+    global browser
+    global getmp
+    global flt1
+    global mp_use1
+    global mp_use1_km
+    global mp_Carrier
+    ## 2-Get caster sourcetable
+    browser = (NtripBrowser(config.caster, port=config.port,
+    timeout=10,coordinates=(config.rlat,config.rlon),
+    maxdist=config.maxdist ))
+    getmp= browser.get_mountpoints()
+    flt = getmp['str']
+    # Purge list
+    flt1 = []
+    ## filter carrier L1-L2
+    flt1 = [m for m in flt if int(m['Carrier'])>=2]
+    ## GET nearest mountpoint
+    for i, value in enumerate(flt1):
+        ## Get first row
+        if i == 0 :
+            ## LOG Nearest base available
+            mp_use1 = value["Mountpoint"]
+            mp_use1_km = value["Distance"]
+            mp_Carrier = value["Carrier"]
+            print(
+                "INFO: Nearest base is ",mp_use1,
+                round(mp_use1_km,2),"km; Carrier:",mp_Carrier)
+            print(
+                "INFO: Distance between Rover & connected base ",
+                config.mp_use,round(config.dist_r2mp,2),"km")
+    ## Value on connected base
+    flt_r2mp = [m for m in flt if m['Mountpoint']==config.mp_use]
+    ## GET distance between rover and mountpoint used.
+    for r in flt_r2mp:
+        config.dist_r2mp = r["Distance"]
+        config.mp_alive = r['Mountpoint']
+    ## LOG Watch all nearests mountpoints
+    # for i in flt:
+    #     mp = i["Mountpoint"]
+    #     di = round(i["Distance"],2)
+    #     car = i["Carrier"]
+    #     print(mp,di,"km; Carrier:", car)
+
+## 03-START loop to check base alive + rover position and nearest base
 def loop_mp():
     while True:
         try:
             global mp_use1
             global mp_use1_km
             global msg
-            ## 1-Analyse nmea from gnss ntripclient for get lon lat
-            ##TODO after x min reset parameters
-            line = config.sio.readline()
-            msg = pynmea2.parse(line)
-            ## Exclude bad longitude
-            if msg.longitude != config.lon:
-                ## LOG coordinate from Rover
-                latlon= msg.latitude, msg.longitude
-                print("ROVER: ",latlon,msg.timestamp)
-                ## 2-Get caster sourcetable
-                browser = (NtripBrowser(config.caster, port=config.port,
-                timeout=10,coordinates=(msg.latitude,msg.longitude),
-                maxdist=config.maxdist ))
-                getmp= browser.get_mountpoints()
-                flt = getmp['str']
-                ## LOG Watch all nearests mountpoints
-                # for i in flt:
-                #     mp = i["Mountpoint"]
-                #     di = round(i["Distance"],2)
-                #     car = i["Carrier"]
-                #     print(mp,di,"km; Carrier:", car)
-                # Purge list
-                flt1 = []
-                ## filter carrier L1-L2
-                flt1 = [m for m in flt if int(m['Carrier'])>=2]
-                ## Value on connected base
-                flt_r2mp = [m for m in flt if m['Mountpoint']==config.mp_use]
-                ## GET distance between rover and mountpoint used.
-                for r in flt_r2mp:
-                    config.dist_r2mp = r["Distance"]
-                    config.mp_alive = r['Mountpoint']
-                ## GET nearest mountpoint
-                for i, value in enumerate(flt1):
-                    ## Get first row
-                    if i == 0 :
-                        ## LOG Nearest base available
-                        mp_use1 = value["Mountpoint"]
-                        mp_use1_km = value["Distance"]
-                        mp_Carrier = value["Carrier"]
-                        print(
-                            "INFO: Nearest base is ",mp_use1,
-                            round(mp_use1_km,2),"km; Carrier:",mp_Carrier)
-                        print(
-                            "INFO: Distance between Rover & connected base ",
-                            config.mp_use,round(config.dist_r2mp,2),"km")
-                        ## Check if it is necessary to change the base
-                        ## Base Alive?
-                        if config.mp_use == config.mp_alive:
-                            print("INFO: Base",config.mp_alive," alive")
-                            ## nearest Base is different?
-                            if config.mp_use != mp_use1:
-                                ## Check Critical distance before change ?
-                                if config.dist_r2mp > config.mp_km_crit:
-                                    ##Hysteresis(htrs)
-                                    crithtrs = config.mp_km_crit + config.htrs
-                                    if config.dist_r2mp < crithtrs:
-                                        print(
-                                            "INFO: Hysteresis running: "
-                                            ,crithtrs,"km")
-                                    else:
-                                        movetobase()
+            ##Get data from Caster
+            ntripbrowser()
+            ##My base is Alive?
+            flt_basealive = [m for m in flt1 if m['Mountpoint']==config.mp_alive]
+            if len(flt_basealive) == 0:
+                print("INFO: Base ",config.mp_alive," is DEAD!")
+                movetobase()
+                logging()
+            if config.mp_use != mp_use1:
+                print("INFO: A closer base is now available")
+                movetobase()
+                logging()
+            else:
+                print("INFO: Connected to ",config.mp_use,", Waiting for the rover's geographical coordinates")
+                ## 1-Analyse nmea from gnss ntripclient for get lon lat
+                ##TODO after x min reset parameters
+                line = config.sio.readline()
+                msg = pynmea2.parse(line)
+                ## Exclude bad longitude
+                if msg.longitude != config.lon:
+                    ## LOG coordinate from Rover
+                    config.rlat = msg.latitude
+                    config.rlon = msg.longitude
+                    config.rtime = msg.timestamp
+                    print("ROVER: ",config.rlat,config.rlon,msg.timestamp)
+                    ## 2-Get caster sourcetable
+                    ntripbrowser()
+                    ### Check if it is necessary to change the base
+                    ## nearest Base is different?
+                    if config.mp_use != mp_use1:
+                        ## Check Critical distance before change ?
+                        if config.dist_r2mp > config.mp_km_crit:
+                            ##critique + Hysteresis(htrs)
+                            crithtrs = config.mp_km_crit + config.htrs
+                            if config.dist_r2mp < crithtrs:
+                                print("INFO: Hysteresis critique running: ",crithtrs,"km")
+                            else:
+                                ##middle mount point 2 mount point hysteresis
+                                r2mphtrs = mp_use1_km + config.htrs
+                                if config.dist_r2mp < r2mphtrs:
+                                    print("INFO: Hysteresis MP 2 MP running: ",r2mphtrs,"km")
                                 else:
-                                    print(
-                                        "INFO:",mp_use1,
-                                        " nearby: ",round(config.dist_r2mp,2),
-                                        " But critical distance not reached: ",
-                                        config.mp_km_crit,"km")
-                            if config.mp_use == mp_use1:
-                                print("INFO: Always connected to ",mp_use1)
+                                    movetobase()
+                                    logging()
+                                    telegrambot()
+
                         else:
-                            print("INFO: Base dead")
-                            movetobase()
+                            print(
+                                "INFO:",mp_use1," nearby: ",round(config.dist_r2mp,2),
+                                " But critical distance not reached: ",config.mp_km_crit,"km")
+                    if config.mp_use == mp_use1:
+                        print("INFO: Always connected to ",mp_use1)
+
         except serial.SerialException as e:
             #print('Device error: {}'.format(e))
             continue
         except pynmea2.ParseError as e:
             #print('Parse error: {}'.format(e))
             continue
+
+def main():
+    ## 00-START socat
+    ## TODO
+    ## 01-START caster
+    Process(target=str2str_out).start()
+    ## 02-START a generic stream RTCM3 in
+    bashstr = config.stream1+config.mp_use+config.stream2
+    str2str = subprocess.Popen(bashstr.split())
+    ## 4-Get str2str in pid
+    config.pid_str = str2str.pid
+    print("STR2STR: Defaut ntripcli 2 serial is runnig, pid: ",config.pid_str)
+    ## 03-START loop to check rover position and nearest base
+    Process(target=loop_mp).start()
 
 if __name__ == '__main__':
     main()
